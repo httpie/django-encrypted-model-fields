92489592d38e6db33d964d2d15e373540c97394e
Jonathan DEKHTIAR <contact@jonathandekhtiar.eu>
2018-08-04 12:49:30 +0000

    Deprecation Warning Fix: RemovedInDjango30Warning: Remove the context parameter from EncryptedCharField.from_db_value(). Support for it will be removed in Django 3.0.

--- encrypted_model_fields/fields.py
+++ encrypted_model_fields/fields.py
@@ -76,7 +76,7 @@ class EncryptedMixin(object):
 
         return super(EncryptedMixin, self).to_python(value)
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, expression, connection):
         return self.to_python(value)
 
     def get_db_prep_save(self, value, connection):
